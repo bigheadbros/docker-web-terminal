0752b09492b1c7c458814066996b53a8f0f87f15
liwei <liwei2633@163.com>
2019-07-22 11:52:57 +0800

    front

---
+++ front/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <index></index>
+  </div>
+</template>
+
+<script>
+import Index from "./components/Index.vue";
+
+export default {
+  name: "app",
+  components: {
+    Index
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ front/src/components/Index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div ref="terminal"></div>
+</template>
+
+<script>
+import "xterm/dist/xterm.css";
+import "xterm/dist/addons/fullscreen/fullscreen.css";
+
+import { Terminal } from "xterm";
+import * as fit from "xterm/lib/addons/fit/fit";
+import * as fullscreen from "xterm/lib/addons/fullscreen/fullscreen";
+import * as webLinks from "xterm/lib/addons/webLinks/webLinks";
+import * as attach from "xterm/lib/addons/attach/attach";
+
+export default {
+  name: "Index",
+  created() {
+    Terminal.applyAddon(attach);
+    Terminal.applyAddon(fit);
+    Terminal.applyAddon(fullscreen);
+    Terminal.applyAddon(webLinks);
+
+    const terminal = new Terminal();
+    const ws = new WebSocket("ws://127.0.0.1:8000/terminal?container=test");
+    ws.onclose = function() {
+      console.log("服务器关闭了连接");
+    };
+    terminal.open(this.$refs.terminal);
+    terminal.fit();
+    terminal.toggleFullScreen();
+    terminal.webLinksInit();
+    terminal.attach(ws);
+  }
+};
+</script>
